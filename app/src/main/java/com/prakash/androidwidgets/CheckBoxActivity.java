4a489442a88ce2149c5327991d8ecc6744b84ff6
Omprakash <omprakash.mopuri@improve10x.com>
2023-01-26 15:35:14 +0530

    Divided Ui and Business logic coding in CheckBoxActivity.java

--- app/src/main/java/com/prakash/androidwidgets/CheckBoxActivity.java
+++ app/src/main/java/com/prakash/androidwidgets/CheckBoxActivity.java
@@ -21,13 +21,30 @@ public class CheckBoxActivity extends AppCompatActivity {
 
     private void handleOrder() {
         binding.orderBtn.setOnClickListener(view -> {
-            if (binding.coffeeCb.isChecked()) {
-                Toast.makeText(this, "coffee 10Rs", Toast.LENGTH_SHORT).show();
-            } else if(binding.pizzaCb.isChecked()) {
-                Toast.makeText(this, "pizza 100Rs", Toast.LENGTH_SHORT).show();
-            } else if (binding.teaCb.isChecked()) {
-                Toast.makeText(this, "tea 8Rs", Toast.LENGTH_SHORT).show();
-            }
+            boolean isPizzaSelected = binding.pizzaCb.isChecked();
+            boolean isCoffeeSelected = binding.coffeeCb.isChecked();
+            boolean isTeaSelected = binding.teaCb.isChecked();
+            String orderDetails = createOrder(isPizzaSelected, isCoffeeSelected, isTeaSelected);
+            Toast.makeText(this, orderDetails, Toast.LENGTH_SHORT).show();
         });
+    }
+
+    private String createOrder(boolean pizza, boolean coffee, boolean tea) {
+        int totalAmount = 0;
+        String orderDetails = "Selected Items:";
+        if (pizza) {
+            totalAmount += 100;
+            orderDetails += "\nPizza Rs.100";
+        }
+        if (coffee) {
+            totalAmount += 50;
+            orderDetails += "\nCoffe Rs.50";
+        }
+        if (tea) {
+            totalAmount += 25;
+            orderDetails += "\nTea Rs.25";
+        }
+        orderDetails += "\nTotal Rs: " + totalAmount;
+        return orderDetails;
     }
 }
---
+++ app/src/main/java/com/prakash/androidwidgets/ImageSliderActivity.java
@@ -0,0 +1,14 @@
+package com.prakash.androidwidgets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageSliderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_slider);
+    }
+}
